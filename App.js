c70ae28380b1c5aa6b19e9cafbea9d25e3caffc0
indulekha-123 <67064107+indulekha-123@users.noreply.github.com>
2020-12-06 10:49:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import HomeScreen from './Screens/HomeScreen';
+
+
+
+export default class App extends React.Component {
+    render() {
+      return (
+        <View>
+        
+        
+         </View>
+      );
+    }
+  }
---
+++ Screen/HomeScreen.js
@@ -0,0 +1,173 @@
+import React,{Component}from 'react';
+import {StyleSheet,View,Text,TextInput,TouchableOpacity}from 'react-native';
+import {Header} from'react-native-elements';
+
+
+
+
+
+export default class HomeScreen extends React.Component{
+
+  getWord=(word)=>{
+ var searchKeyword=word.toLowerCase()
+ var url ="https://rupinwhitehatjr.github.io/dictionary/%22+searchKeyword+%22.json"+searchKeyword+".json"
+ return fetch(url)
+ .then((data)=>{
+     if(data.status===200){
+         return data.json()
+     }
+     else{
+         return null
+     }
+ })
+
+
+ .then((response)=>{
+     var responseObject = response
+     if(responseObject){
+         var wordData = responseObject.definitions[0]
+         var definition=wordData.description
+         var lexicalCategory=wordData.wordtype
+
+         this.setState({
+             "word" : this.state.text,
+             "definition": definition,
+             "lexicalCategory": lexicalCategory
+
+
+     
+         })
+        
+     }
+
+     else {
+         this.setState({
+             "word": this.state.text,
+             "definition":'Not Found',
+
+         })
+
+
+     }
+    })
+
+}
+render(){
+    return(
+
+<TextInput
+style={styles.inputBox}
+onChangeText={text =>{
+    this.setState({
+        text: text,
+        isSearchPressed:false,
+        word : "Loading..."
+        lexicalCategory:'',
+        examples :[],
+        defination:""
+    });
+}}
+value={this.state.text}/>
+
+
+
+ <TouchableOpacity
+style={styles.searchButton}
+onPress={()=>{
+    this.setState({isSearchPressed :true});
+    this.getWord(this.state.text)
+}}>
+    
+</TouchableOpacity>
+
+
+<View style={style.detailsContainer}>
+    <Text style ={styles.detailsTitle}>
+        Word:{""}
+    </Text>
+<Text style={{frontSize:18}}>
+    {this.state.word}
+</Text>
+</View>
+
+
+<View style={{flexDirection:'row',flexWrap:'wrap'}}>
+    <Text style={styles.detailsTitle}>
+        Definition:{""}
+
+    </Text>
+
+<Text style={{ fontSize:18}}>
+    {this.state.definition}
+</Text>
+
+</View>
+
+<View style={styles.outputContainer}>
+    <Text style={{fontSize:20}}>
+        {
+            this.state.isSearchPressed&& this.state.word==="Loading...."
+            ?this.state.word
+            :""
+        }
+    </Text>
+    {
+        this.state.word!=="Loading..."?
+        (
+            <View style={{justifyContent:'center',marginLeft:10}}>
+
+        )
+    }
+</View>
+
+
+<View style={styles.detailsContainer}>
+    <Text style={styles.detailsTitle}>
+        Word:{""}
+    </Text>
+    <Text style={{frontSize:18}}>
+      {this.state.word}  
+
+    </Text>
+</View>
+
+
+
+<View style={styles.detailsContainer}>
+    <Text style={styles.detailsTitle}>
+        Type:{""}
+    </Text>
+
+    <Text style={{frontSize:18}}>
+        {this.state.lexicalCategory}
+    </Text>
+</View>
+
+
+        );}
+
+
+}
+
+
+const styles =StyleSheet.create({
+    container:{
+        flex: 1,
+
+    },
+    inputBoxContainer:{
+        flex:0.3,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    inputBox:{
+        width: "80%",
+        alignSelf: 'center',
+        height:40
+    }
+    
+});
+
+
+
+
